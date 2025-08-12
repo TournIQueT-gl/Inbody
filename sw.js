d8a8878ab57a91072b939829e942ed55f664c9b8
TournIQueT-gl <viocex0@gmail.com>
2025-08-12 12:54:22 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,25 @@
-// Service Worker v3 placeholder
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => clients.claim());
+const CACHE = 'inbody-ultra-cache-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request).then(resp => {
+      return caches.open(CACHE).then(cache => { cache.put(e.request, resp.clone()); return resp; });
+    }).catch(() => caches.match('./index.html')))
+  );
+});
